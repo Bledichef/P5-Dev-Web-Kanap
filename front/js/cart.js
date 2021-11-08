1aab0e65cfcaf9a10c1eb521922cca0351f1dc7e
Guillaume C <81625005+Bledichef@users.noreply.github.com>
2021-11-08 11:45:32 +0100

    Add files via upload

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,16 +2,18 @@ let ProduitsValide = JSON.parse(localStorage.getItem("Produits")) || [];
 
 
 
-
 for (let sofa of ProduitsValide){
 
+
+let ProduitDejaPresent = (sofa.idChoisie == ProduitsValide.idChoisie & sofa.ColorChoisie == ProduitsValide.ColorChoisie);
+if(ProduitDejaPresent) {
+ document.getElementsByName("itemQuantity").innerHTML +=` 
+ value="${sofa.quantite}+${ProduitsValide.quantite}"`
+}
   
 
-
-    document.getElementById("cart__items").innerHTML =`
-    
-    
-
+else{
+    document.getElementById("cart__items").innerHTML +=`
 
       <article class="cart__item" data-id=${sofa.idChoisie}>
     <div class="cart__item__img">
@@ -25,8 +27,8 @@ for (let sofa of ProduitsValide){
       <div class="cart__item__content__settings">
       <p>Couleur : ${sofa.ColorChoisie}</p>
         <div class="cart__item__content__settings__quantity">
-          <p>Qté :${sofa.quantite} </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+          <p>Qté : ${sofa.quantite} </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${sofa.quantite}">
         </div>
         <div class="cart__item__content__settings__delete">
           <p class="deleteItem">Supprimer</p>
@@ -35,4 +37,7 @@ for (let sofa of ProduitsValide){
     </div>
   </article> 
     `
+    console.log("Les articles dans le  panier : ", ProduitsValide);
 }
+}
+
