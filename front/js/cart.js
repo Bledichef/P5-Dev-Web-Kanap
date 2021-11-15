92a79973edf6c74f160be120e68ea477c99c5e25
Guillaume C <81625005+Bledichef@users.noreply.github.com>
2021-11-15 16:10:09 +0100

    Add files via upload

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,47 +1,9 @@
 let ProduitsValide = JSON.parse(localStorage.getItem("Produits")) ;
   
 
+
 ////////////////////////////// Visibilité des elements dans le panier 
-/*for (let sofa of ProduitsValide){
 
-
-let ProduitDejaPresent = (sofa.idChoisie == ProduitsValide.idChoisie & sofa.ColorChoisie == ProduitsValide.ColorChoisie);
-let TotalElementPanier = (sofa.quantite+ProduitsValide.quantite);
-
-if(ProduitDejaPresent) {
- document.getElementsByName("itemQuantity").innerHTML +=` 
- <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${TotalElementPanier}"`
-}
-  
-
-else{
-    document.getElementById("cart__items").innerHTML +=`
-
-      <article class="cart__item" data-id=${sofa.idChoisie}>
-    <div class="cart__item__img">
-      <img src=${sofa.imageChoisie} alt=${sofa.AltTxtChoisie}>
-    </div>
-    <div class="cart__item__content">
-      <div class="cart__item__content__titlePrice">
-        <h2>${sofa.NomChoisie}</h2>
-        <p>${sofa.PrixChoisie} €</p>
-      </div>
-      <div class="cart__item__content__settings">
-      <p>Couleur : ${sofa.ColorChoisie}</p>
-        <div class="cart__item__content__settings__quantity">
-          <p>Qté : ${sofa.quantite} </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${sofa.quantite}">
-        </div>
-        <div class="cart__item__content__settings__delete">
-          <p class="deleteItem">Supprimer</p>
-        </div>
-      </div>
-    </div>
-  </article> 
-    `
-    console.log("Les articles dans le  panier : ", ProduitsValide);
-}
-}*/
 
 
 
@@ -62,7 +24,7 @@ if (ProduitsValide ===null || ProduitsValide ==0) {
   
     
 
-      <article class="cart__item" data-id=${ProduitsValide[i].idChoisie}>
+      <article class="cart__item" data-id=${ProduitsValide[i].idChoisie} data-color="${ProduitsValide[i].ColorChoisie}">
     <div class="cart__item__img">
       <img src=${ProduitsValide[i].imageChoisie} alt=${ProduitsValide[i].AltTxtChoisie}>
     </div>
@@ -74,7 +36,7 @@ if (ProduitsValide ===null || ProduitsValide ==0) {
       <div class="cart__item__content__settings">
       <p>Couleur : ${ProduitsValide[i].ColorChoisie}</p>
         <div class="cart__item__content__settings__quantity">
-          <p>Qté : ${ProduitsValide[i].quantite} </p>
+          <p>Qté :  </p>
           <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${ProduitsValide[i].quantite}">
         </div>
         <div class="cart__item__content__settings__delete">
@@ -89,10 +51,14 @@ if (ProduitsValide ===null || ProduitsValide ==0) {
   
 
 
+  
+                                         // QUANTITE TOTAL DANS LE PANIER 
 
-                                         // qUANTITE TOTAL DANS LE PANIER 
-const arrayQuantity = [];
-for (let sofa of ProduitsValide){
+       
+   
+  
+  const arrayQuantity = [];
+   for (let sofa of ProduitsValide){
     arrayQuantity.push(sofa.quantite)
     const reducer = (previousValue, currentValue) => previousValue + currentValue;
     const totalQuantity = arrayQuantity.reduce(reducer)
@@ -101,12 +67,14 @@ for (let sofa of ProduitsValide){
 }
 
 if(arrayQuantity.length === 0){
-    document.querySelector("h1").innerHTML = "Le panier est vide";
+    document.querySelector("h1").innerHTML = "Panier vide";
     totalQuantity = "";
     document.getElementById("totalQuantity").innerHTML = totalQuantity;
 }
-
+ 
 ///////////////////////////////////// Prix total
+
+function prixToltal(ProduitsValide) {
 let totalPriceQuantity = "";
 let arrayPrice = [];
 
@@ -121,33 +89,86 @@ if(arrayPrice.length === 0){
     totalPriceQuantity = "";
     document.getElementById("totalPrice").innerHTML = totalPriceQuantity;
 }
-
+}
 
 //////////////////////////////////////// DELETE ARTICLE
- ProduitsValide => {
-  const tagDelete = document.querySelectorAll(".deleteItem"); 
+ /*
+  const Delete = document.querySelectorAll(".deleteItem"); 
   
-  tagDelete.forEach(tag => {
+  Delete.forEach(tag => {
       const tagClosest = tag.closest("article");
+      console.log(tagClosest)
       const id = tagClosest.dataset.id; 
+      console.log(id)
       const article = tagClosest;
+      console.log(article)
       const color = tagClosest.dataset.color;
+      console.log(color)
   
       tag.addEventListener('click', event => {
           event.preventDefault();
           ProduitsValide.forEach(sofa => {
+            console.log(sofa)
+            console.log(sofa.idChoisie)
+            console.log(sofa.ColorChoisie)
               if (sofa.idChoisie === id && sofa.ColorChoisie === color){ 
                   let index = ProduitsValide.indexOf(sofa) // récupération index du canapé
                   if(confirm("Voulez vous supprimer cet article de votre panier?")){
                       article.remove(); // supprime du DOM
-                      productInStorage.splice(index, 1); // on retire ce canapé du panier
+                      ProduitsValide.splice(index, 0); // on retire ce canapé du panier
                   }
               }
           })
 
-      localStorage.setItem("products", JSON.stringify(productInStorage)); 
 
-      displayTotalPrice(ProduitsValide);
-      displayTotalQuantity(ProduitsValide);        
-      })
-  })}}
+      localStorage.setItem("Produits", JSON.stringify(ProduitsValide)); 
+
+     // displayTotalPrice(ProduitsValide);
+      //displayTotalQuantity(ProduitsValide);        
+        })
+  })
+*/
+
+
+
+///////////////////////////////////////////////////////////////////////// MODIF QUANTITY 
+
+
+const tagQuantity = document.querySelectorAll(".itemQuantity");
+
+tagQuantity.forEach(tag => {
+    const tagClosest = tag.closest("article");
+    let newQuantity = "";
+    const id = tagClosest.dataset.id; 
+    const color = tagClosest.dataset.color;
+console.log(tagClosest)
+console.log(id)
+    tag.addEventListener('change', event => {
+        event.preventDefault();
+        newQuantity = Number(tag.value); // la nouvelle quantité est la value de la balise quantité
+        ProduitsValide.forEach(sofa => { // Pour chaque canapé mis ds le panier, si l'id est le même que celui récupéré -> on cible le canapé 
+            
+            if (sofa.idChoisie === id  && sofa.ColorChoisie === color){ 
+                sofa.quantite = newQuantity // la quantité des produits du panier se met à jour et devient égale à la nouvelle quantité
+                
+            }
+            console.log(id)
+            console.log(sofa.idChoisie)
+            console.log(color)
+            console.log(sofa.ColorChoisie)
+            console.log(newQuantity)
+            console.log(sofa)
+            
+        })
+
+    localStorage.setItem("Produits", JSON.stringify(ProduitsValide)); 
+    prixToltal(ProduitsValide)
+ 
+    })
+})
+}
+
+
+prixToltal(ProduitsValide)
+
+
