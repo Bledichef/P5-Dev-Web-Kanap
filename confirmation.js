640a59b5e81e9ca6c738e42af2a1d6b6b98f6fdc
Guillaume C <81625005+Bledichef@users.noreply.github.com>
2021-11-28 19:43:13 +0100

    Delete confirmation.js

--- confirmation.js
+++
@@ -1,7 +0,0 @@
-let tagOrderId = document.querySelector("#orderId");
- console.log(tagOrderId)
- console.log(orderId)
-  const urlConfirmation = window.location.search; 
-   const urlSearchParams = new URLSearchParams(urlConfirmation);
-    tagOrderId.innerHTML = urlSearchParams.get('orderId'); //récupère la clé orderId et l'insère dans le span
-     localStorage.clear(); // vide le localStorage
